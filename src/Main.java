b15bac97895f76045def693dc6d066c6aa2584a0
matu2209 <matu.2209@gmail.com>
2024-03-23 14:02:20 -0300

    Se agrego toString a todas las clases (mejora la vista), se agrega test de clases en el main.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import Cliente.Cliente;
+import Producto.*;
+import Cliente.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Se crean productos y se muestran
+        Teclado tecladoGamer = new Teclado(36000,7,"Teclado Gamer","Logitech",true,false);
+        Teclado tecladoGenerico = new Teclado(18999,10,"Teclado Generico","Lenovo",false,true);
+        Mouse muoseGamer = new Mouse(21999,4,"Mouse Gamer","Logitech",true,true);
+        Silla sillaOficina = new Silla(125000,3,"Silla Oficina Clasica","Home Office","Cuero");
+        Pantalla monitorGamer = new Pantalla(255000,4,"Monitor Gamer","Asus",27);
+
+        System.out.println(tecladoGamer);
+        System.out.println(monitorGamer);
+        System.out.println(tecladoGenerico);
+        System.out.println(muoseGamer);
+        System.out.println(sillaOficina);
+
+
+        //creamos usuario y tambien los mostramos
+        Cliente c1 = new Cliente("Matias","Galvan","matu@gmail.com");
+        Cliente c2 = new Cliente("Rosana","Cardoso","roxi@hotmail.com");
+
+        System.out.println(c1);
+        System.out.println(c2);
+
+
+
+
+
+
     }
 }
